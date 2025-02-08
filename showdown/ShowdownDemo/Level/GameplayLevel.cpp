05b7b5e8219d12eda8f05883699344df2d71c617
mangryang <mangryang.dev@gmail.com>
2025-02-09 04:56:29 +0900

    FIX : Modify POV to change according to menu selection
     - Modifying the constructor of ChessplayLevel, JanggiplayLevel
     - Modify the data type of the member variable "board" to an int-type two-dimensional array

--- showdown/ShowdownDemo/Level/GameplayLevel.cpp
+++ showdown/ShowdownDemo/Level/GameplayLevel.cpp
@@ -1,19 +1,5 @@
 #include "GameplayLevel.h"
 #include "Engine/Engine.h"
-#include "Actor/ChessPiece/King.h"
-#include "Actor/ChessPiece/Queen.h"
-#include "Actor/ChessPiece/Rook.h"
-#include "Actor/ChessPiece/Bishop.h"
-#include "Actor/ChessPiece/Knight.h"
-#include "Actor/ChessPiece/Pawn.h"
-#include "Actor/ChessPiece/Jester.h"
-#include "Actor/JanggiPiece/Emperor.h"
-#include "Actor/JanggiPiece/Cannon.h"
-#include "Actor/JanggiPiece/Elephant.h"
-#include "Actor/JanggiPiece/Horse.h"
-#include "Actor/JanggiPiece/Chariot.h"
-#include "Actor/JanggiPiece/Soldier.h"
-#include "Actor/JanggiPiece/Guard.h"
 
 /*
 * Chess pieces
@@ -35,78 +21,15 @@ Janggi pieces
 13: Gaurd (\u58EB)
 */
 
-static Vector2 BoardPositionToLocation(int xpos, int ypos) {
-    return Vector2((xpos * 4) + 2, ypos * 2 + 1);
-}
-
 GameplayLevel::GameplayLevel()
 {
-    board = {
-        {11, 9, 10, 13, -1, 13, 9, 10, 11},
-        {-1, -1, -1, -1, 7, -1, -1, -1, -1},
-        {-1, 8, -1, -1, -1, -1, -1, 8, -1},
-        {12, -1, 12, -1, 12, -1, 12, -1, 12},
-        {-1, -1, -1, -1, -1, -1, -1, -1, -1},
-        {-1, -1, -1, -1, -1, -1, -1, -1, -1},
-        {-1, -1, -1, -1, -1, -1, -1, -1, -1},
-        {5, 5, 5, 5, 5, 5, 5, 5, 5},
-        {2, 4, 3, 6, 0, 6, 4, 3, 2}
-    };
-
     for (int row = 0; row < 9; ++row)
     {
         for (int col = 0; col < 9; ++col)
         {
-            switch (board[row][col])
-            {
-            case -1:
-                break;
-            case 0: // King
-                Level::AddActor(new King(BoardPositionToLocation(col, row)));
-                break;
-            case 2: // Rook
-                Level::AddActor(new Rook(BoardPositionToLocation(col, row)));
-                break;
-            case 3: // Bishop
-                Level::AddActor(new Bishop(BoardPositionToLocation(col, row)));
-                break;
-            case 4: // Knight
-                Level::AddActor(new Knight(BoardPositionToLocation(col, row)));
-                break;
-            case 5: // Pawn
-                Level::AddActor(new Pawn(BoardPositionToLocation(col, row)));
-                break;
-            case 6: // Jester
-                Level::AddActor(new Jester(BoardPositionToLocation(col, row)));
-                break;
-            case 7: // Emperor
-                Level::AddActor(new Emperor(BoardPositionToLocation(col, row)));
-                break;
-            case 8: // Cannon
-                Level::AddActor(new Cannon(BoardPositionToLocation(col, row)));
-                break;
-            case 9: // Elephant
-                Level::AddActor(new Elephant(BoardPositionToLocation(col, row)));
-                break;
-            case 10: // Horse
-                Level::AddActor(new Horse(BoardPositionToLocation(col, row)));
-                break;
-            case 11: // Chariot
-                Level::AddActor(new Chariot(BoardPositionToLocation(col, row)));
-                break;
-            case 12: // Soldier
-                Level::AddActor(new Soldier(BoardPositionToLocation(col, row)));
-                break;
-            case 13: // Guard
-                Level::AddActor(new Guard(BoardPositionToLocation(col, row)));
-                break;
-
-            default:
-                break;
-            }
+            board[row][col] = -1;
         }
     }
-    Level::ProcessAddedAndDestroyedActor();
 }
 
 GameplayLevel::~GameplayLevel()
@@ -142,4 +65,9 @@ void GameplayLevel::Render()
             Engine::Get().Render(Vector2(28 + ix * 4, i * 2 - 1), "¦¢", Color::White);
         }
     }
+}
+
+Vector2 GameplayLevel::BoardPositionToLocation(int xpos, int ypos)
+{
+    return Vector2((xpos * 4) + 2, ypos * 2 + 1);
 }
